4b0ed89c8580b69687e28e45741fb6e74b2b831f
Bluzzi <camilledugas.pro@gmail.com>
2022-12-20 16:14:53 +0100

    main (storybook): change story path

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   "stories": [
     "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.story.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
