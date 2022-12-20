16ec6c9be3333859ea2003bdf97c3beb955eef3c
Bluzzi <camilledugas.pro@gmail.com>
2022-12-20 15:30:40 +0100

    storybook (config): use typescript

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,6 @@
-const path = require('path');
-module.exports = {
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
   "stories": [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
@@ -17,3 +18,5 @@ module.exports = {
     "docsPage": true
   }
 }
+
+export default config;
