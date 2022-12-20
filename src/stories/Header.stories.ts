7ea84bad0ff70327c7ace1e13f86c4b543ddf484
Bluzzi <camilledugas.pro@gmail.com>
2022-12-20 15:31:50 +0100

    Lint

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -1,15 +1,15 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Header } from './Header';
+import type { Meta, StoryObj } from "@storybook/react";
+import { Header } from "./Header";
 
 const meta = {
-  title: 'Example/Header',
+  title: "Example/Header",
   component: Header,
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/7.0/react/writing-docs/docs-page
-  tags: ['docsPage'],
+  tags: ["docsPage"],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
-    layout: 'fullscreen',
-  },
+    layout: "fullscreen"
+  }
 } satisfies Meta<typeof Header>;
 
 export default meta;
@@ -18,9 +18,9 @@ type Story = StoryObj<typeof meta>;
 export const LoggedIn: Story = {
   args: {
     user: {
-      name: 'Jane Doe',
-    },
-  },
+      name: "Jane Doe"
+    }
+  }
 };
 
 export const LoggedOut: Story = {};
